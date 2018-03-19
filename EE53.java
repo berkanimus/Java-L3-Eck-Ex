b6c1e8ffc3ac734505a16a7edb597aa0e6923ad8
berkanimus <berkanimuss@gmail.com>
2018-03-19 21:41:04 +0100

    EE53

---
+++ EE53.java
@@ -0,0 +1,36 @@
+
+public class DiceRollStats2 {
+
+   private int NUMBER_OF_EXPERIMENTS = 10000;
+   private  PairOfDice dice = new PairOfDice();
+           
+      // return number of attempt to get given number n 
+      // : N > 1 
+    public static int rollFor( int N ) {
+        int occ = 0;  // Occurence made to get number N.
+        while (dice.getTotal()!=N) {
+          dice.roll();
+          occ++;
+        } 
+        return occ;
+   }
+   
+    // Do the experience rollFor NUMBER_OF_EXPERIMENTS times for each possible total of two dice
+    public static void resultForEachTotal () {
+       for ( int total = 2;  total <= 12;  total++ ) {
+          StatCalc stats = new StatCalc(); // object variable to ge
+          // Do the experience rollFor NUMBER_OF_EXPERIMENTS times 
+          for ( int i = 0; i < NUMBER_OF_EXPERIMENTS; i++ ) {
+              stats.enter( rollFor(total) );
+          }
+         System.out.printf("For the total ", total, "mean is ", stats.getMean(), "standar deviation is " stats.getStandardDeviation(), " and the largest item is" stats.getMax());    
+   } 
+
+
+
+   public static void main(String[] args) {
+   
+     private  PairOfDice test = new PairOfDice();
+     test.resultForEachTotal(); // test the function 
+   }  
+}  
